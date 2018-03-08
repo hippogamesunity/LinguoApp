e427af9d27daed5bf045e673b90636282e28ed62
Nate River <hippogamesunity@gmail.com>
2018-03-08 21:35:00 +0300

    upload

---
+++ LinguoApp/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LinguoApp
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			textBox2.Text = Transtator.Translate(textBox1.Text);
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ LinguoApp/Transtator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LinguoApp
+{
+	public static class Transtator
+	{
+		private static Dictionary<char, char> SimpleReplace = new Dictionary<char, char>
+		{
+			{ 'Ą', 'Ѫ' }, { 'a', 'ѫ' },
+
+		};
+
+		public static string Translate(string input)
+		{
+			var result = input;
+
+			foreach (var entry in SimpleReplace)
+			{
+				result = result.Replace(entry.Key, entry.Value);
+			}
+
+			return result;
+		}
+	}
+}
